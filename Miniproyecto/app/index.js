ba78eeccbf845edd078efa4820c39703a01a44eb
sarang3232 <72816187+sarang3232@users.noreply.github.com>
2022-10-21 18:10:02 -0500

    Add files via upload

---
+++ Miniproyecto/app/index.js
@@ -0,0 +1,46 @@
+const Consul = require('consul');
+const express = require('express');
+
+const SERVICE_NAME='web_service';
+const SERVICE_ID='m'+process.argv[2];
+const SCHEME='http';
+const HOST='192.168.100.5';
+const PORT=process.argv[2]*1;
+const PID = process.pid;
+
+/* Inicializacion del server */
+const app = express();
+const consul = new Consul();
+
+app.get('/health', function (req, res) {
+    console.log('Health check!');
+    res.end( "Ok." );
+    });
+
+app.get('/', (req, res) => {
+  console.log('GET /', Date.now());
+  res.json( "Este es el servidor 2");
+});
+
+app.listen(PORT, function () {
+    console.log('Servicio iniciado en:'+SCHEME+'://'+HOST+':'+PORT+'!');
+    });
+
+/* Registro del servicio */
+var check = {
+  id: SERVICE_ID,
+  name: SERVICE_NAME,
+  address: HOST,
+  port: PORT, 
+  check: {
+	   http: SCHEME+'://'+HOST+':'+PORT+'/health',
+	   ttl: '5s',
+	   interval: '5s',
+     timeout: '5s',
+     deregistercriticalserviceafter: '1m'
+	   }
+  };
+ 
+consul.agent.service.register(check, function(err) {
+  	if (err) throw err;
+  	});
